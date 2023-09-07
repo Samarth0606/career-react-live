79d763db876ebf2d2614bab175d37c60f94642d2
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-09-07 22:10:21 +0530

    lecture 1 codes

---
+++ Lecture-1/first-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // js ❌
+    // jsx -> js + html ✅
+    // single parent
+    <div>
+
+      <div>kaise ho sab? khana khaakr jaana</div>
+      <h1>aao jam peeya jaae</h1>
+
+    </div>
+
+  )
+}
+
+export default App
